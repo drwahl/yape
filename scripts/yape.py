84398c06f371ec2dd02d3a6f9ed4a143a33a5a2d
David Wahlstrom <david.wahlstrom@memorylane.com>
2013-01-02 10:34:26 -0800

    add some debug flags to yape.py and yape_node_classifier.py

--- scripts/yape.py
+++ scripts/yape.py
@@ -24,12 +24,25 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
 
+import sys
+import logging
+
+logging.basicConfig(level=logging.WARN,
+                    format='%(asctime)s %(levelname)s - %(message)s',
+                    datefmt='%y.%m.%d %H:%M:%S'
+                   )
+console = logging.StreamHandler(sys.stderr)
+console.setLevel(logging.WARN)
+logging.getLogger("yape").addHandler(console)
+log = logging.getLogger("yape")
 
 class Node(object):
     """Class to facilitate adding/modifying a node"""
+    log.debug('in class Node()')
 
     def __init__(self, nodename, nodeclass=None, classparams=None, nodeparam=None, puppet_inherit=None, environment=None):
         """Initialize variables for use"""
+        log.debug('in Node().__init__')
 
         #create an initial connection to the mongodb
         self.mongo_collection = self.configure()
@@ -58,6 +71,7 @@ class Node(object):
 
     def configure(self):
         """Read configuration file and intialize connection to the mongodb instance"""
+        log.debug('in Node().configure')
 
         from pymongo import Connection
         import os
@@ -65,36 +79,47 @@ class Node(object):
 
         parser = SafeConfigParser()
         if os.path.isfile('/etc/yape/conf.ini'):
+            log.debug('reading config from: /etc/yape/conf.ini')
             config = '/etc/yape/conf.ini'
         else:
+            log.debug('reading config from: ../conf/conf.ini')
             config = os.path.join(os.path.dirname(__file__), "../conf/conf.ini")
         parser.read(config)
-        database = parser.get('mongodb_info', 'mongodb_db_name')
-        collection = parser.get('mongodb_info', 'mongodb_collection_name')
         host = parser.get('mongodb_info', 'mongodb_server')
+        log.debug('connecting to mongodb host: %s' % host)
+        database = parser.get('mongodb_info', 'mongodb_db_name')
+        log.debug('connecting to database name: %s' % database)
+        collection = parser.get('mongodb_info', 'mongodb_collection_name')
+        log.debug('using collection name: %s' % collection)
         con = Connection(host)
         col = con[database][collection]
         return col
 
     def verifynode(self, vernode=None):
         """Verify that the node exists in the DB. Returns True if it does exist, Return False if it does not"""
+        log.debug('in Node().verifynode')
 
         if not vernode:
             testnode = self.node
         else:
             testnode = vernode
 
+        log.debug('verifying node: %s' % testnode)
         docnode = self.mongo_collection.find_one({'node': testnode})
         if docnode is None:
+            log.debug('node %s not found in database' % testnode)
             return False
         else:
             if docnode['node'] == testnode:
+                log.debug('node %s found in database' % testnode)
                 return True
             else:
+                log.debug('node %s not found in database' % testnode)
                 return False
 
     def parse_node_classification(self, puppet_class=None, class_params=None, parameters=None, environment=None, inherit=None):
         """Parse puppet_class, class_params, parameters, and environment, and return a dict with the result"""
+        log.debug('in Node().parse_node_classification')
 
         puppet_enc = {
             'classes': '',
@@ -133,8 +158,11 @@ class Node(object):
 
         paramclass = {}
         try:
+            log.debug('trying to get class parameters')
             paramclass = self.mongo_collection.find_one({'node': self.node})['enc']['classes']
+            log.debug('got class parameters: %s' % paramclass)
         except:
+            log.debug('unable to get class parameters (not a parameterised class?)')
             paramclass = {}
 
         if not paramclass:
@@ -142,55 +170,76 @@ class Node(object):
 
         #Pull parameters for each class
         if puppet_class:
+            log.debug('processing puppet class: %s' % puppet_class)
             paramkeyvalue = {}
             paramvalue = []
             if not class_params:
+                log.debug('no class parameters for class: %s' % puppet_class)
                 paramkeyvalue = ''
             else:
+                log.debug('parameters found for class: %s' % puppet_class)
                 for param in class_params.split(','):
+                    log.debug('processing parameter "%s" for class "%s"' % (param, puppet_class))
                     paramkey = ''
                     paramkey = param.split('=')[0]
+                    log.debug('found parameter key to be: %s' % paramkey)
                     paramvalue.append(param.split('=')[1])
+                    log.debug('found parameter value to be: %s' % paramvalue)
                 paramkeyvalue[paramkey] = paramvalue
 
+            log.debug('setting class "%s" with key/value: %s' % (puppet_class, paramkeyvalue))
             paramclass[puppet_class] = paramkeyvalue
 
             #Since we stored them as lists above, reduce single item lists to strings
             for puppetclass in paramclass:
+                log.debug('cleaning up class: %s' % puppetclass)
                 for param in paramclass[puppetclass]:
+                    log.debug('cleaning up parameter: %s' % param)
                     if type(paramclass[puppetclass][param]) is type(list()):
+                        log.debug('processing %s as a list')
                         if len(paramclass[puppetclass][param]) == 1:
+                            log.debug('%s is a list with a single entry')
                             paramclass[puppetclass][param] = paramclass[puppetclass][param][0]
 
             puppet_enc['classes'] = paramclass
 
+        log.debug('returning puppet_enc as: %s' % puppet_enc)
         return puppet_enc
 
     def update(self, enc, inherit=None):
         """Add/modify "enc" properties on "node" """
+        log.debug('in Node().update')
 
         if inherit:
+            log.debug('inherit passed to Node().update as %s' % inherit)
             tmp_inherit = inherit
         else:
+            log.debug('inherit not passed to Node().update. using: %s' % self.puppet_inherit)
             tmp_inherit = self.puppet_inherit
 
         if not self.verifynode('none'):
+            log.debug('adding node "none" to mongodb server: {\'node\': \'none\'}, {"$set": {\'enc\': {\'classes\': []}, \'inherit\': ''}}, True')
             self.mongo_collection.update({'node': 'none'}, {"$set": {'enc': {'classes': []}, 'inherit': ''}}, True)
 
+        log.debug('sending update to mongodb server: {\'node\': %s}, {"$set": {\'enc\': %s, \'inherit\': %s}}, True' % (self.node, enc, tmp_inherit))
         self.mongo_collection.update({'node': self.node}, {"$set": {'enc': enc, 'inherit': tmp_inherit}}, True)
 
     def remove(self, rmnode=None):
         """Remove node from mongodb"""
+        log.debug('in Node().remove')
 
         if rmnode:
+            log.debug('rmnode passed to Node().remove as: %s' % rmnode)
             nodename = rmnode
         else:
+            log.debug('rmnode not passed to Node().remove. using: %s' % self.node)
             nodename = self.node
 
         nodes_with_inheritance = self.mongo_collection.find({'inherit': nodename})
         for inheritnode in nodes_with_inheritance:
-            print "%s inherits the node you are trying to remove." % inheritnode['node']
+            print "%s inherits the node you are removing." % inheritnode['node']
 
+        log.debug('sending remove to mongodb server: {\'node\': %s}' % nodename)
         self.mongo_collection.remove({'node': nodename})
 
 if __name__ == "__main__":
@@ -205,11 +254,17 @@ if __name__ == "__main__":
     cmd_parser.add_argument('-p', '--param', dest='puppet_param', help='Apply (or remove with -r) parameters (global variables) to node (-n).', action='store', default=None)
     cmd_parser.add_argument('-i', '--inherit', dest='puppet_inherit', help='Apply (or remove with -r) inherit node.  Only a single node can be set to inherit from.', action='store', default=None)
     cmd_parser.add_argument('-e', '--environment', dest='environment', help='Apply (or remove with -r) puppet agent environment.', default=None)
+    cmd_parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='Enable debugging during execution', default=None)
     args = cmd_parser.parse_args()
+
+    if args.debug:
+        log.setLevel(logging.DEBUG)
 
     node = Node(args.puppet_node, args.puppet_class, args.class_params, args.puppet_param, args.puppet_inherit, args.environment)
 
     if args.remove_attr is True:
+        log.debug('remove attributes requested')
         node.remove(node.node)
     else:
+        log.debug('node update requested')
         node.update(node.parse_node_classification())
--- scripts/yape_node_classifier.py
+++ scripts/yape_node_classifier.py
@@ -23,11 +23,23 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 :email: david.wahlstrom@gmail.com
 
 """
+
 import yaml
 import sys
+import logging
+
+logging.basicConfig(level=logging.WARN,
+                    format='%(asctime)s %(levelname)s - %(message)s',
+                                        datefmt='%y.%m.%d %H:%M:%S'
+                                                           )
+console = logging.StreamHandler(sys.stderr)
+console.setLevel(logging.WARN)
+logging.getLogger("yape_node_classifier").addHandler(console)
+log = logging.getLogger("yape_node_classifier")
 
 def configure():
     """Read configuration file and intialize connection to the mongodb instance"""
+    log.debug('in configure()')
 
     from pymongo import Connection
     import os
@@ -35,51 +47,63 @@ def configure():
 
     parser = SafeConfigParser()
     if os.path.isfile('/etc/yape/conf.ini'):
+        log.debug('loaded config file from: /etc/yape/conf.ini')
         config = '/etc/yape/conf.ini'
     else:
+        log.debug('loaded config file from: ../conf/conf.ini')
         config = os.path.join(os.path.dirname(__file__), "../conf/conf.ini")
     parser.read(config)
-    database = parser.get('mongodb_info', 'mongodb_db_name')
-    collection = parser.get('mongodb_info', 'mongodb_collection_name')
     host = parser.get('mongodb_info', 'mongodb_server')
+    log.debug('connecting to mongodb host: %s' % host)
+    database = parser.get('mongodb_info', 'mongodb_db_name')
+    log.debug('connecting to database name: %s' % database)
+    collection = parser.get('mongodb_info', 'mongodb_collection_name')
+    log.debug('using collection name: %s' % collection)
     con = Connection(host)
     col = con[database][collection]
     return col
 
 def classify(cnode):
     """Classify the requested node, including inheritances"""
+    log.debug('in classify(%s)' % cnode)
 
     col = configure()
     try:
+        log.debug('attempting to retrieve classes for %s' % cnode)
         node_classes = col.find_one({'node': cnode})['enc']['classes']
+        log.debug('classes found for %s: %s' % (cnode, node_classes))
     except TypeError:
+        log.debug('unable to retrieve classes for %s' % cnode)
         node_classes = {}
 
     try:
-        # Grab the info from the inheritance node
+        log.debug('grabbing info from the inheritance node')
         inode = col.find_one({'node': cnode})
         if not inode['inherit']:
+            log.debug('no inheritance node found')
             raise KeyError
+        log.debug('found inheritance node: %s' % inode)
         inode_classes = classify(inode['inherit'])
         if not col.find_one({"node" : cnode}):
             print "ERROR: Inheritance node " + cnode + " not found in ENC"
             sys.exit(1)
-        node_classes = col.find_one({"node": cnode})['enc']['classes']
-        # Grab the requested node's classes
-        tmp_class_store = node_classes
-        # Apply the inheritance node classes
+        tmp_class_store = col.find_one({"node": cnode})['enc']['classes']
+        log.debug('retrieved classes for %s: %s' % (cnode, tmp_class_store))
         node_classes = dict(inode_classes)
-        # Apply the requested node's classes and overrides
+        log.debug('setting inheritance node (%s) classes: %s' % (inode['inherit'], dict(inode_classes)))
         node_classes.update(tmp_class_store)
+        log.debug('replaying requested nodes classes on top of inheritance node classes: %s' % node_classes)
     except KeyError:
         pass 
     except TypeError:
         pass 
 
+    log.debug('returning node_classes as: %s' % node_classes)
     return node_classes
 
 def main(node):
     """This script is called by puppet"""
+    log.debug('in main(%s)' % node)
 
     if (len(sys.argv) < 2):
         print "ERROR: Please supply a hostname or FQDN"
@@ -100,4 +124,11 @@ def main(node):
 
 
 if __name__ == "__main__":
+
+    try:
+        if sys.argv[2] == '-d':
+            log.setLevel(logging.DEBUG)
+    except:
+        pass
+
     main(sys.argv[1])
